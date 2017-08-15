2c4f0f5047280a39e67171f3acac9f7bad3f6558
Igor Fontana <igor193@gmail.com>
2017-08-15 11:12:01 -0300

    Verificando se nome já está em uso na troca de nome

--- imports/api/jogadores/methods.js
+++ imports/api/jogadores/methods.js
@@ -12,6 +12,11 @@ export const trocarNome = new ValidatedMethod({
       throw new Meteor.Error('usuario.nao.logado', 'Usuário não está logado');
     }
 
-    Jogadores.update({userId: this.userId}, {$set: {nome: nome, redefinirNome: false}});
+    const jogador = Jogadores.findOne({nome});
+    if (jogador) {
+      throw new Meteor.Error('jogador.nome.existente', 'Este nome está sendo usado por alguém');
+    }
+
+    Jogadores.update({userId: this.userId}, {$set: {nome, redefinirNome: false}});
   }
 });
